dca071a01b99c02b62b5afd0b8caecb3feb31c25
raulyypunto <raul_stb@hotmail.com>
2022-09-13 03:00:15 +0200

    incluyendo funciones en la animacion

--- Animacion/ani.js
+++ Animacion/ani.js
@@ -6,8 +6,23 @@ anime({
     loop: true,
     direction: 'alternate',
     easing: 'easeInElastic(1, .6)',
-    delay: (el, i) => i * 100
-  });
+    delay: 350,
+    rotate: {
+      value: 360,
+      duration: 1800,
+      easing: 'easeInOutSine'
+    },
+    scale: {
+      value: 2,
+      duration: 1600,
+      delay: 800,
+      easing: 'easeInOutQuart'
+    },
+    borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
+    scale: function(el, i, l) {
+      return (2*l - 10*i) + .25;
+  }
+});
 
   var pepe = setTimeout(()=>{
     var p = document.getElementById("fuera");
