95c6fbe1af3c8741c8b43568977da64a26a66c5c
raulyypunto <raul_stb@hotmail.com>
2022-09-16 15:37:54 +0200

    Añadiendo la animacion a pagina principal y comentandolo

--- Animacion/ani.js
+++ Animacion/ani.js
@@ -1,13 +1,12 @@
-var carga = document.querySelectorAll("temp");
 anime({
   targets: ".caja input",
   value: [0, 100],
   round: 1,
   easing: 'easeInOutExpo',
-  duration: 3500,
+  duration: 1900,
   direction: 'alternate'
 });
  var pepe = setTimeout(()=>{
   var p = document.getElementById("fuera");
   p.remove();
- },3700);
+ },2000);
