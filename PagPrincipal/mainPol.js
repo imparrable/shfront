9d315b84bf806c5b57e22f137790f256429d8794
Pol Lazaro <pol.lazaro.rosas@gmail.com>
2022-09-16 17:18:45 +0200

    juntando front

--- PagPrincipal/mainPol.js
+++ PagPrincipal/mainPol.js
@@ -10,22 +10,22 @@ let cambioweb2 = document.getElementById("contact");
 
 
 //un evento para cada boton, actuando como un "a" pero es un formulario para pasar info
-cambioweb1.addEventListener("click", () => {
-  document.getElementById("resultado").value = ciudadUrl;
-  document.getElementById("punto").value = codnUrl;
-  document.getElementById("form2").setAttribute('action', '../MasCercano/index.html');
-  document.getElementById("form2").submit();
-  alert("coordenada: "+codnUrl);
-});
+// cambioweb1.addEventListener("click", () => {
+//   document.getElementById("resultado").value = ciudadUrl;
+//   document.getElementById("punto").value = codnUrl;
+//   document.getElementById("form2").setAttribute('action', '../MasCercano/index.html');
+//   document.getElementById("form2").submit();
+//   alert("coordenada: "+codnUrl);
+// });
 
-cambioweb2.addEventListener("click", () => {
-  document.getElementById("resultado").value = ciudadUrl;
-  document.getElementById("punto").value = codnUrl;
-  document.getElementById("form2").setAttribute('action', '../Contacto/index.html');
-  document.getElementById("form2").submit();
-});
+// cambioweb2.addEventListener("click", () => {
+//   document.getElementById("resultado").value = ciudadUrl;
+//   document.getElementById("punto").value = codnUrl;
+//   document.getElementById("form2").setAttribute('action', '../Contacto/index.html');
+//   document.getElementById("form2").submit();
+// });
 //generacion del HTML
-fetch("https://4le956zpa7.execute-api.us-west-2.amazonaws.com/sh-api1/services?city="+ciudadUrl)
+fetch(("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Salud"))
   .then((data) => data.json())
   .then((services) => showServices(services));
 
@@ -37,14 +37,14 @@ function showServices(services) {
         ${service.name}
         <div class="callButton">
         <button type="button" class="buttonPhone">
-         <a href="tel:${service.telephone}"><img src="./iconos/telefono (1).png" height="41px" width="40px"></a>
+         <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
         </button>
          </a>
        </div>
      </div>`;
       });
     }
-      fetch("https://4le956zpa7.execute-api.us-west-2.amazonaws.com/sh-api1/services?city=Barcelona&category=Seguridad")
+    fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Seguridad")
 
   .then((data) => data.json())
   .then((services) => showServices2(services));
@@ -57,33 +57,34 @@ function showServices(services) {
                  ${service.name}
                  <div class="callButton">
                  <button type="button" class="buttonPhone2">
-                  <a href="tel:${service.telephone}"><img src="./iconos/telefono (1).png" height="41px" width="40px"></a>
+                  <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
                  </button>
                   </a>
                 </div>
               </div>`;        
        });
     }
-    fetch("https://4le956zpa7.execute-api.us-west-2.amazonaws.com/sh-api1/services?city=Barcelona&category=Reparaciones")
+    fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Reparaciones")
 
     .then((data) => data.json())
     .then((services) => showServices3(services));
-       function showServices3(services) {
-      services.forEach((service) => {
-        const container3 = document.querySelector(".desplegable3");
-         container3.innerHTML += ` 
-             <div class="links3">
-             ${service.name}
-             <div class="callButton">
-             <button type="button" class="buttonPhone3">
-              <a href="tel:${service.telephone}"><img src="./iconos/telefono (1).png" height="41px" width="40px"></a>
-             </button>
-              </a>
-            </div>
-          </div>`; 
-    });
-  }
-    fetch("https://4le956zpa7.execute-api.us-west-2.amazonaws.com/sh-api1/services?city=Barcelona&category=Mascotas")
+  
+      function showServices3(services) {
+      const container3 = document.querySelector(".desplegable3");
+         services.forEach((service) => {
+             container3.innerHTML += ` 
+                 <div class="links3">
+                   ${service.name}
+                   <div class="callButton">
+                   <button type="button" class="buttonPhone3">
+                    <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
+                   </button>
+                    </a>
+                  </div>
+                </div>`;        
+         });
+      }
+      fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Mascotas")
 
     .then((data) => data.json())
     .then((services) => showServices4(services));
@@ -95,7 +96,7 @@ function showServices(services) {
            ${service.name}
            <div class="callButton">
            <button type="button" class="buttonPhone4">
-            <a href="tel:${service.telephone}"><img src="./iconos/telefono (1).png" height="41px" width="40px"></a>
+            <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
            </button>
             </a>
           </div>
