8557aa15633f31cd78003840b9e8bdbc627be831
raulyypunto <raul_stb@hotmail.com>
2022-09-19 02:25:44 +0200

    añadiendo funcionalidades asíncronas

--- PagPrincipal/mainPol.js
+++ PagPrincipal/mainPol.js
@@ -1,8 +1,5 @@
-
-var ciudadUrl = document.getElementById("resultado").value;
 var codnUrl = document.getElementById("punto").value;
 var id_carta = 0;
-console.log(ciudadUrl+" -- "+codnUrl);
 let cambioweb1 = document.getElementById("mascerca");
 let cambioweb2 = document.getElementById("contact");
 //formulario.setAttribute('action', 'ventas/registrar_compra') (EJEMPLO)
@@ -24,81 +21,115 @@ let cambioweb2 = document.getElementById("contact");
 //   document.getElementById("form2").submit();
 // });
 //generacion del HTML
+async function Comienzo() {
+  navigator.geolocation.getCurrentPosition(geoposOK);
+  function geoposOK(pos){
+    //Obtenemos latitud y longitud
+    const lat = pos.coords.latitude;
+    const long = pos.coords.longitude;
+    ciudad_actual(lat,long);    
+  }
+  function ciudad_actual(cdn1,cdn2){
+    var obtenido;
+    var urlinfo = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${cdn1},${cdn2}&key=${KEY}`;
+    fetch(urlinfo)
+      .then(response => response.json())
+        .then(data => {
+              let parts = data.results[0].address_components;
+              parts.forEach(element => {
+                  if(element.types.includes("locality")){
+                      //codificamos la respuesta por problemas con los espacios y apostrofes para nuestra api:
+                      city_result = element.long_name.replaceAll(" ","").replaceAll("L'","L");
+                      //rellenamos el formulario con el municipio y lo enviamos a nuestra api.
+                      //console log para verificar cuanto correcto es.
+                      console.log(city_result);
+                      obtenido = city_result;
+                      //console.log para verificar lo que obtienen las variables y los cambios realizados.
+                  }
+                });
+            });
+    funcion_creada("barcelona");
+  }
+
+}
+
+async function funcion_creada(ciudadUrl){
 fetch(("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Salud"))
   .then((data) => data.json())
   .then((services) => showServices(services));
 
-function showServices(services) {
-  var container = document.querySelector(".desplegable");
-         services.forEach((service) => {
-    container.innerHTML += ` 
-        <div class="links">
-        ${service.name}
-        <div class="callButton">
-        <button type="button" class="buttonPhone">
-         <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
-        </button>
-         </a>
-       </div>`;
-      });
-      container.innerHTML += "<div class='links'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone'><a href='../MasCercano/index.html?servicio=hospital'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
-    }
-    fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Seguridad")
-
-  .then((data) => data.json())
-  .then((services) => showServices2(services));
-
-    function showServices2(services) {
-    var container2 = document.querySelector(".desplegable2");
-       services.forEach((service) => {
-           container2.innerHTML += ` 
-               <div class="links2">
-                 ${service.name}
-                 <div class="callButton">
-                 <button type="button" class="buttonPhone2">
-                  <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
-                 </button>
-                  </a>
-                </div>`;        
-       });
-       container2.innerHTML += "<div class='links2'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone2'><a href='../MasCercano/index.html?servicio=police'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
-    }
-    fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Reparaciones")
-
-  .then((data) => data.json())
-  .then((services) => showServices3(services));
-
-    function showServices3(services) {
-    var container3 = document.querySelector(".desplegable3");
-       services.forEach((service) => {
-           container3.innerHTML += ` 
-               <div class="links3">
-                 ${service.name}
-                 <div class="callButton">
-                 <button type="button" class="buttonPhone3">
-                  <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
-                 </button>
-                  </a>
-                </div>`;        
-       });
-       container3.innerHTML += "<div class='links3'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone3'><a href='../MasCercano/index.html?servicio=locksmith'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
-    }
-      fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Mascotas")
+  function showServices(services) {
+    var container = document.querySelector(".desplegable");
+          services.forEach((service) => {
+      container.innerHTML += ` 
+          <div class="links">
+          ${service.name}
+          <div class="callButton">
+          <button type="button" class="buttonPhone">
+          <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
+          </button>
+          </a>
+        </div>`;
+        });
+        container.innerHTML += "<div class='links'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone'><a href='../MasCercano/index.html?servicio=hospital'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
+      }
+      fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Seguridad")
 
     .then((data) => data.json())
-    .then((services) => showServices4(services));
-     function showServices4(services) {
-      var container4 = document.querySelector(".desplegable4");
-      services.forEach((service) => {
-       container4.innerHTML += ` 
-           <div class="links4">
-           ${service.name}
-           <div class="callButton">
-           <button type="button" class="buttonPhone4">
-            <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
-           </button>
-            </a>
-          </div>`;   
- });
- container4.innerHTML += "<div class='links4'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone4'><a href='../MasCercano/index.html?servicio=veterinary_care'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
+    .then((services) => showServices2(services));
+
+      function showServices2(services) {
+      var container2 = document.querySelector(".desplegable2");
+        services.forEach((service) => {
+            container2.innerHTML += ` 
+                <div class="links2">
+                  ${service.name}
+                  <div class="callButton">
+                  <button type="button" class="buttonPhone2">
+                    <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
+                  </button>
+                    </a>
+                  </div>`;        
+        });
+        container2.innerHTML += "<div class='links2'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone2'><a href='../MasCercano/index.html?servicio=police'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
+      }
+      fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Reparaciones")
+
+    .then((data) => data.json())
+    .then((services) => showServices3(services));
+
+      function showServices3(services) {
+      var container3 = document.querySelector(".desplegable3");
+        services.forEach((service) => {
+            container3.innerHTML += ` 
+                <div class="links3">
+                  ${service.name}
+                  <div class="callButton">
+                  <button type="button" class="buttonPhone3">
+                    <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
+                  </button>
+                    </a>
+                  </div>`;        
+        });
+        container3.innerHTML += "<div class='links3'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone3'><a href='../MasCercano/index.html?servicio=locksmith'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
+      }
+        fetch("https://euvgxet430.execute-api.eu-west-3.amazonaws.com/services?city="+ciudadUrl+"&category=Mascotas")
+
+      .then((data) => data.json())
+      .then((services) => showServices4(services));
+      function showServices4(services) {
+        var container4 = document.querySelector(".desplegable4");
+        services.forEach((service) => {
+        container4.innerHTML += ` 
+            <div class="links4">
+            ${service.name}
+            <div class="callButton">
+            <button type="button" class="buttonPhone4">
+              <a href="tel:${service.telephone}"><img src="./iconos/telefono.png" height="41px" width="40px"></a>
+            </button>
+              </a>
+            </div>`;   
+  });
+  container4.innerHTML += "<div class='links4'><strong>Los más cercanos</strong><div class='callButton'><button type='button' class='buttonPhone4'><a href='../MasCercano/index.html?servicio=veterinary_care'><img src='../MasCercano/map-marker.png' height='41px' width='40px'></a></button></a></div></div>";
+  }
 }
